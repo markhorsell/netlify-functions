24647c3acc3d5f714cc19cd7e33b795f6985170d
Mark Horsell <mark.horsell@gmail.com>
2021-01-19 17:32:11 +0000

    try weather

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 NETLIFY SERVERLESS TEST
+
+see https://naughty-engelbart-6b8874.netlify.app/.netlify/functions/hello
---
+++ functions/weather.ts
@@ -0,0 +1,65 @@
+const axios = require("axios");
+
+exports.handler = async event => {
+
+
+    //
+    const getWeather = async () => {
+    try {
+     
+        //forecast/hourly
+        const resp = await axios.get(
+         "https://api.openweathermap.org/data/2.5/forecast?q=Cirencester&appid=" +
+           process.env["WEATHER"]+
+           '&units=metric'
+       );
+   
+         // console.log(resp.data)
+         return resp.data;
+       } catch (error) { 
+         console.log(error.response.body);
+       }
+     };
+     const data = await getWeather();
+ 
+
+
+    return {
+      statusCode: 200,
+      body: data
+    }
+  }
+
+
+
+
+  /*
+    context.log('HTTP trigger function processed a request.');
+    const name = (req.query.name || (req.body && req.body.name));
+   
+    const ip=req.headers['x-forwarded-for']?.split(".")?.slice(0,3)?.join(".") || "no";
+
+
+
+    
+    if(ip=="209.93.245"){
+        context.res = {
+            
+            body: ip+"https://api.openweathermap.org/data/2.5/weather?q=Cirencester,uk&APPID="+process.env['WEATHER']
+        }; 
+        return;
+    }
+   
+    const responseMessage = name
+        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
+        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+
+    context.res = {
+        // status: 200, /* Defaults to 200 
+        body: process.env['WEATHER']//req//.headers['x-forwarded-for']//+ip//responseMessage
+    };
+   
+};
+
+export default httpTrigger;
+*/
